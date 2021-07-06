21583e934153388b308fdc6d636cb42d360bde1b
Muhammad-Haris-Ali <harismusharaf9001@gmail.com>
2021-07-06 06:44:09 -0700

    Responsive side menu in mobile

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,56 +1,95 @@
-<div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow no-bt-margin">
-        <h5 class="my-0 mr-md-auto font-weight-normal"><a class="title-anchor" href="{{ route('index') }}">Vigilant Trading Group</a>
-        </h5>
-            <nav class="navbar navbar-expand-lg my-2 my-md-0 mr-md-3">
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="nav-item active">
-                            <a class="p-2 text-dark" href="{{ route('disclaimer') }}">Disclaimer</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="p-2 text-dark" href="#">Blog</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="p-2 text-dark" href="#">Reviews</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="p-2 text-dark" href="{{ route('pricing') }}">Pricing</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="p-2 text-dark" href="{{ route('about') }}">About Us</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="p-2 text-dark" href="{{ route('index') }}">Home</a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-            <a class="btn btn-outline-primary" href="{{ route('support') }}">Support</a>
+<div
+    class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow no-bt-margin">
+    <h5 class="my-0 mr-md-auto font-weight-normal row ">
+        <button data-trigger="navbar_main" class="d-lg-none btn btn-warning" type="button"><span
+                class="navbar-toggler-icon"></span></button>
+        <a class="title-anchor" style="margin-top: 8px;" href="{{ route('index') }}">Vigilant Trading Group</a>
+    </h5>
 
-            @if(Auth::user())
-            <li class="nav-item dropdown" style="list-style: none;margin-left: 5px;">
-                <a id="navbarDropdown" class="btn btn-outline-primary" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                    {{ Auth::user()->name }}
+
+    <span class="screen-darken"></span>
+    {{--            <nav class="navbar navbar-expand-lg my-2 my-md-0 mr-md-3">--}}
+    {{--                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">--}}
+    {{--                    <span class="navbar-toggler-icon"></span>--}}
+    {{--                </button>--}}
+    {{--                <div class="collapse navbar-collapse" id="navbarNav">--}}
+    {{--                    <ul class="navbar-nav">--}}
+    {{--                        <li class="nav-item active">--}}
+    {{--                            <a class="p-2 text-dark" href="{{ route('disclaimer') }}">Disclaimer</a>--}}
+    {{--                        </li>--}}
+    {{--                        <li class="nav-item">--}}
+    {{--                            <a class="p-2 text-dark" href="#">Blog</a>--}}
+    {{--                        </li>--}}
+    {{--                        <li class="nav-item">--}}
+    {{--                            <a class="p-2 text-dark" href="#">Reviews</a>--}}
+    {{--                        </li>--}}
+    {{--                        <li class="nav-item">--}}
+    {{--                            <a class="p-2 text-dark" href="{{ route('pricing') }}">Pricing</a>--}}
+    {{--                        </li>--}}
+    {{--                        <li class="nav-item">--}}
+    {{--                            <a class="p-2 text-dark" href="{{ route('about') }}">About Us</a>--}}
+    {{--                        </li>--}}
+    {{--                        <li class="nav-item">--}}
+    {{--                            <a class="p-2 text-dark" href="{{ route('index') }}">Home</a>--}}
+    {{--                        </li>--}}
+    {{--                    </ul>--}}
+    {{--                </div>--}}
+    {{--            </nav>--}}
+    <nav id="navbar_main" class="mobile-offcanvas navbar navbar-expand-lg navbar-dark">
+        <div class="container-fluid">
+            <div class="offcanvas-header text-right">
+                <button class="btn-close float-end">X</button>
+            </div>
+            <a class="navbar-brand" href="#" style="color: black">Menus</a>
+
+            <ul class="navbar-nav">
+                <li class="nav-item mt-2 active">
+                    <a class="p-2 text-dark" href="{{ route('disclaimer') }}">Disclaimer</a>
+                </li><br>
+                <li class="nav-item mt-2">
+                    <a class="p-2 text-dark" href="#">Blog</a>
+                </li>
+                <br>
+                <li class="nav-item mt-2">
+                    <a class="p-2 text-dark" href="#">Reviews</a>
+                </li>
+                <br>
+                <li class="nav-item mt-2">
+                    <a class="p-2 text-dark" href="{{ route('pricing') }}">Pricing</a>
+                </li>
+                <br>
+                <li class="nav-item mt-2">
+                    <a class="p-2 text-dark" href="{{ route('about') }}">About Us</a>
+                </li>
+                <br>
+                <li class="nav-item mt-2">
+                    <a class="p-2 text-dark" href="{{ route('index') }}">Home</a>
+                </li>
+                <a class="btn btn-outline-primary" href="{{ route('support') }}">Support</a>
+            </ul>
+
+        </div> <!-- container-fluid.// -->
+    </nav>
+
+
+    @if(Auth::user())
+        <li class="nav-item dropdown" style="list-style: none;margin-left: 5px;">
+            <a id="navbarDropdown" class="btn btn-outline-primary" href="#" role="button" data-toggle="dropdown"
+               aria-haspopup="true" aria-expanded="false" v-pre>
+                {{ Auth::user()->name }}
+            </a>
+
+            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="{{ route('logout') }}"
+                   onclick="event.preventDefault();
+                                        document.getElementById('logout-form').submit();">
+                    {{ __('Logout') }}
                 </a>
 
-                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="{{ route('logout') }}"
-                        onclick="event.preventDefault();
-                                        document.getElementById('logout-form').submit();">
-                        {{ __('Logout') }}
-                    </a>
-
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                        @csrf
-                    </form>
-                </div>
-            </li>
-            <!-- <a class="btn btn-outline-primary" href="{{ route('support') }}">{{ Auth::user()->name }}</a> -->
-        <!-- <a class="btn btn-outline-primary" href="{{ route('support') }}">Support</a> -->
-
-
-        @endif
-    </div>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                    @csrf
+                </form>
+            </div>
+        </li>
+    @endif
+</div>
